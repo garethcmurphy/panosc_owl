9daff72e77560cb7d92bec26a9d1c918db1a2477
Gareth Murphy <garethcmurphy@gmail.com>
2019-10-16 13:26:36 +0200

    add ontology

--- panosc_owl.py
+++ panosc_owl.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 from owlready2 import *
-owlready_ontology = get_ontology("http://confluence.panosc.eu/owlready_ontology.owl").load()
+
+onto = get_ontology("http://test.org/onto.owl")
+
+class Drug(Thing):
+    namespace = onto
