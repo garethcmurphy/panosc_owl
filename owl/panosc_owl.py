ad0442152d3c5c8526ab7673a379023b1e2b982d
Gareth Murphy <garethcmurphy@gmail.com>
2019-10-16 14:01:49 +0200

    add reflectometry :rocket:

--- owl/panosc_owl.py
+++ owl/panosc_owl.py
@@ -23,6 +23,15 @@ def main():
     class Imaging(Techniques):
         """class emission"""
 
+    class IonSpectroscopy(Techniques):
+        """class emission"""
+
+    class Lithography(Techniques):
+        """class emission"""
+
+    class Scattering(Techniques):
+        """class emission"""
+
     exafs = Absorption("Extended X-Ray Absorption Fine Structure (EXAFS)")
     exafs = Absorption("IR spectroscopy")
     exafs = Absorption("Near edge X-ray absorption fine structure (NEXAFS)")
@@ -30,7 +39,23 @@ def main():
     exafs = Absorption("Ultraviolet circular dichroism (UVCD)")
     exafs = Absorption("X-ray magnetic circular dichroism (XMCD)")
 
+    exafs = Diffraction("Ellipsometry")
+    exafs = Diffraction("Micro XRF")
+    exafs = Diffraction("Polarimetry")
+    exafs = Diffraction("Reflectometry ")
+    exafs = Diffraction("Time-resolved studies")
+    exafs = Diffraction("X-ray excited optical luminescence (XEOL)")
+    exafs = Diffraction("X-ray fluorescence (XRF)")
+
     exafs = Emission("Ellipsometry")
+
+    exafs = Imaging("Ellipsometry")
+
+    exafs = IonSpectroscopy("Ellipsometry")
+
+    exafs = Lithography("Ellipsometry")
+
+    exafs = Scattering("Ellipsometry")
 
 
     onto.save()
